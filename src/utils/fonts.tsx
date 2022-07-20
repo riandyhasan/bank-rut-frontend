aefc577eedd250ca2c2720f6fc9a7369b87b674a
Yandy <riandyhsn@gmail.com>
2022-07-20 16:51:48 +0700

    add fonts

---
+++ src/utils/fonts.tsx
@@ -0,0 +1,11 @@
+const fonts = {
+    bold: 'Poppins-Bold',
+    extraBold: 'Poppins-ExtraBold',
+    light: 'Poppins-Light',
+    medium: 'Poppins-Medium',
+    thin: 'Poppins-Thin',
+    regular: 'Poppins-Regular',
+    italic: 'Poppins-Italic',
+}
+
+export default fonts;
