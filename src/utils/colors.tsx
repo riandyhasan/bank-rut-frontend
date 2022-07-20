e62245649b2408aa44d32a5fd88a44965325f2f4
Yandy <riandyhsn@gmail.com>
2022-07-20 16:51:42 +0700

    add colors

---
+++ src/utils/colors.tsx
@@ -0,0 +1,14 @@
+const colors  = {
+    transparent: 'transparent',
+    white: '#FFFFFF',
+    black: '#000000',
+    primaryBlue: '#2A47AA',
+    orange: '#E9AD79',
+    red: '#DF5C51',
+    green: '#6BBE3F',
+    secondaryBlue: '#8E9ED6',
+    pink: '#FFE7C9',
+    grey: '#575757',
+};
+
+export default colors;
